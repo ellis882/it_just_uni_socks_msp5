ad21301322d65355cc89bbe9744288464b6adfae
ellis882 <ellisrachel1304@me.com>
2022-03-25 08:19:19 +0000

    created views and url for contact page

--- shop/views.py
+++ shop/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def shop(request):
+    """ A view to return the shop page"""
     context = {}
     return render(request, 'shop/shop.html', context)
 
 def cart(request):
+    """ A view to return the cart page"""
     context = {}
     return render(request, 'shop/cart.html', context)
 
 def checkout(request):
+    """ A view to return the checkout page"""
     context = {}
     return render(request, 'shop/checkout.html', context)
