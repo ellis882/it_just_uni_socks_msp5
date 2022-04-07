cfad29877ddd32cb9612b02849f2e6f266741bb0
ellis882 <ellisrachel1304@me.com>
2022-04-07 13:35:55 +0000

    added calculating, query and render totals for cart template

--- shop/models.py
+++ shop/models.py
@@ -48,6 +48,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.transaction_id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     """ is an item within our cart. One order can have multiple orderitems """
@@ -59,6 +71,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.product.name)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
