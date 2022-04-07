a834279f8c7ba8d02de0db12b0ad9e2a5bf9aaa4
ellis882 <ellisrachel1304@me.com>
2022-04-07 11:14:20 +0000

    added models to shop app and render products in views

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
 def shop(request):
     """ A view to return the shop page"""
-    context = {}
+
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'shop/shop.html', context)
+
 
 def cart(request):
     """ A view to return the cart page"""
     context = {}
     return render(request, 'shop/cart.html', context)
+
 
 def checkout(request):
     """ A view to return the checkout page"""
