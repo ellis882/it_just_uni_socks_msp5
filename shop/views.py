5e052e2e8fb48af06f3b3463d2680266a2a4c4de
ellis882 <ellisrachel1304@me.com>
2022-04-12 12:11:17 +0000

    added site functionality to cart, shipping address, checkout form and procesorder

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 
-from .models import Product, Customer, Order, OrderItem
+from .models import Product, Customer, Order, OrderItem, ShippingAddress
 
 # Create your views here.
 
@@ -10,8 +11,20 @@ from .models import Product, Customer, Order, OrderItem
 def shop(request):
     """ A view to return the shop page"""
 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,
+                                                     complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # create empty cart for now for non-logged in users
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'shop/shop.html', context)
 
 
@@ -23,12 +36,14 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer,
                                                      complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # create empty cart for now for non-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order}
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'shop/cart.html', context)
 
 
@@ -40,22 +55,75 @@ def checkout(request):
         order, created = Order.objects.get_or_create(customer=customer,
                                                      complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         # create empty cart for now for non-logged in users
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order}
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'shop/checkout.html', context)
 
 
 def updateItem(request):
     """ if you add an item to the cart"""
 
-    data = json.loads(request.data)
+    data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
 
     print('productId', productId)
     print('Action', action)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer,
+                                                 complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, 
+                                                         product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
+
+#from django.views.decorators.csrf import csrf_exempt
+
+#@csrf_exempt
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,
+                                                 complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not logged in..')    
+    return JsonResponse('Payment complete', safe=False)
