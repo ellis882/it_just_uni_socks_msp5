3ec3349e2c4dfd684cb2d8665964dfcd1f7b100a
ellis882 <ellisrachel1304@me.com>
2022-04-26 12:08:25 +0000

    set cookies and created cart in base and cart.js

--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     """ when a customer order a product """
 
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
